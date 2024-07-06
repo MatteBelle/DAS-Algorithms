3e7db1f891ffa49d820e1c1ea2a98c52239c1537
CipStr <cipdev16@gmail.com>
2024-07-06 23:12:27 +0200

    Updated task1 + added code for ros

--- formation_ros2_ws/src/formation_control/formation_control/the_agent.py
+++ formation_ros2_ws/src/formation_control/formation_control/the_agent.py
@@ -5,6 +5,30 @@ from rclpy.node import Node
 from std_msgs.msg import Float32MultiArray as MsgFloat
 
 
+def grad_function_1(z, r, s, gamma, delta):
+    return 2 * gamma * (z[0] - r[0]) + 2 * delta * (z[0] - s[0]), 2 * gamma * (z[1] - r[1]) + 2 * delta * (z[1] - s[1])
+
+def grad_function_2(z, s, delta):
+    return 2 * delta * (z - s)
+
+def grad_phi(z):
+    return 1
+
+def phi(z):
+    return z
+
+def gradient_tracking(zz_at, ss_at, vv_at, alpha, r, AA, AA_neighbors, vv_at_neighbors, ss_at_neighbors, delta):
+    zz_next = zz_at - alpha * (grad_function_1(zz_at, r, ss_at) + grad_phi(zz_at) * vv_at)
+    ss_next = AA * ss_at
+    vv_next = AA * vv_at
+    for jj in AA_neighbors:
+        vv_next += AA * vv_at_neighbors[jj]
+        ss_next += AA * ss_at_neighbors[jj]
+
+    ss_next += phi(zz_next) - phi(zz_at)
+    vv_next += grad_function_2(zz_next, ss_next, delta) - grad_function_2(zz_at, ss_at, delta)
+    return zz_next, ss_next, vv_next
+
 def formation_control_vector_field(
     DeltaT,
     XX_ii,
@@ -34,8 +58,15 @@ class Agent(Node):
         )
         self.agent_id = self.get_parameter("id").value
         self.neighbors = self.get_parameter("neighbors").value
-        self.x_i = np.array(self.get_parameter("xzero").value)
-        self.dist_ii = np.array(self.get_parameter("dist").value)
+        self.zz_init = self.get_parameter("zz_init").value
+        self.ss_init = self.get_parameter("ss_init").value
+        self.vv_init = self.get_parameter("vv_init").value
+        self.r = self.get_parameter("r").value
+        self.gamma = self.get_parameter("gamma").value
+        self.delta = self.get_parameter("delta").value
+        self.alpha = self.get_parameter("alpha").value
+        self.AA = self.get_parameter("AA").value
+        self.AA_neighbors = self.get_parameter("AA_neighbors").value
         self.maxIters = self.get_parameter("maxT").value
 
         self.get_logger().info(f"I am agent: {self.agent_id}")
@@ -58,23 +89,28 @@ class Agent(Node):
         self.timer = self.create_timer(communication_time, self.timer_callback)
 
         self.received_data = {j: [] for j in self.neighbors}
+        self.vv_at_neighbors = {j: np.zeros_like(self.vv_init) for j in self.neighbors}
+        self.ss_at_neighbors = {j: np.zeros_like(self.ss_init) for j in self.neighbors}
 
     def listener_callback(self, msg):
         j = int(msg.data[0])
         msg_j = list(msg.data[1:])
         self.received_data[j].append(msg_j)
+        self.vv_at_neighbors[j] = np.array(msg_j[2])
+        self.ss_at_neighbors[j] = np.array(msg_j[1])
         return None
 
     def timer_callback(self):
         msg = MsgFloat()
 
         if self.t == 0:
-            msg.data = [float(self.agent_id), float(self.t)] + self.x_i.tolist()
+            msg.data = [float(self.agent_id), float(self.t)] + self.ss_init.tolist() + self.vv_init.tolist()
             self.publisher.publish(msg)
 
-            x_i_string = f"{np.array2string(self.x_i, precision=4, floatmode='fixed', separator=', ')}"
+            ss_to_string = f"{np.array2string(self.ss_init, precision=4, floatmode='fixed', separator=', ')}"
+            vv_to_string = f"{np.array2string(self.vv_init, precision=4, floatmode='fixed', separator=', ')}"
 
-            self.get_logger().info(f"Iter: {self.t} x_{self.agent_id}: {x_i_string}")
+            self.get_logger().info(f"Iter: {self.t} x_{self.agent_id}: {ss_to_string, vv_to_string}")
 
             self.t += 1
         else:
@@ -88,12 +124,17 @@ class Agent(Node):
                 )
 
             if all_received:
-                self.x_i = formation_control_vector_field(
-                    self.DeltaT,
-                    self.x_i,
-                    self.neighbors,
-                    self.received_data,
-                    self.dist_ii,
+                self.zz_init, self.ss_init, self.vv_init = gradient_tracking(
+                    self.zz_init,
+                    self.ss_init,
+                    self.vv_init,
+                    self.alpha,
+                    self.r,
+                    self.AA,
+                    self.AA_neighbors,
+                    self.vv_at_neighbors,
+                    self.ss_at_neighbors,
+                    self.delta,
                 )
 
                 msg.data = [float(self.agent_id), float(self.t)] + self.x_i.tolist()
@@ -113,8 +154,8 @@ class Agent(Node):
 
 
 def main():
+    print("Starting agent")
     rclpy.init()
-
     agent = Agent()
     sleep(1)
     rclpy.spin(agent)
--- formation_ros2_ws/src/formation_control/launch_folder/formation_launch.py
+++ formation_ros2_ws/src/formation_control/launch_folder/formation_launch.py
@@ -1,56 +1,131 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
 import numpy as np
+import networkx as nx
+import numpy as np
 
+def phi(z):
+    return z
+# Step 2: Define the nonlinear transformation function phi
+def sigma(z):
+    return np.mean(phi(z), axis=0)
+def cost_function(z, r, s, gamma, delta):
+    return gamma * np.linalg.norm(z - r)**2 + delta * np.linalg.norm(z - s)**2
+def grad_function_1(z, r, s, gamma, delta):
+    return 2 * gamma * (z[0] - r[0]) + 2 * delta * (z[0] - s[0]), 2 * gamma * (z[1] - r[1]) + 2 * delta * (z[1] - s[1])
+
+def grad_function_2(z, s, delta):
+    return 2 * delta * (z - s)
+
+def grad_phi(z):
+    return 1
 
 def generate_launch_description():
-    NN = 6
-    n_x = 2
-    x_init = np.random.rand(n_x * NN)
 
-    L = 2
-    D = 2 * L
-    distances = np.array(
-        [
-            [0, L, 0, D, 0, L],
-            [L, 0, L, 0, D, 0],
-            [0, L, 0, L, 0, D],
-            [D, 0, L, 0, L, 0],
-            [0, D, 0, L, 0, L],
-            [L, 0, D, 0, L, 0],
-        ]
-    )
+    np.random.seed(0)
+    # Parameters
+    NN = 10  # Number of points
+    d = 2    # Dimension of the input space
+    q = 4
+    gamma = 0.5
+    delta = 0.5
+    # Step 1: Generate a dataset
+    r = np.random.randn(NN, d)
 
-    Adj = distances > 0
+    G = nx.cycle_graph(NN)
 
-    MAXITERS = 200
+    I_NN = np.eye(NN)
+
+    Adj = nx.adjacency_matrix(G)
+    Adj = Adj.toarray()
+    AA = np.zeros(shape=(NN, NN))
+
+    for ii in range(NN):
+        N_ii = np.nonzero(Adj[ii])[0]
+        deg_ii = len(N_ii)
+        for jj in N_ii:
+            deg_jj = len(np.nonzero(Adj[jj])[0])
+            AA[ii, jj] = 1 / (1 + max([deg_ii, deg_jj]))
+
+    AA += I_NN - np.diag(np.sum(AA, axis=0))
+
+    if 0:
+        print(np.sum(AA, axis=0))
+        print(np.sum(AA, axis=1))
+    MAXITERS = 1000
+    dd = 2
+
+    ZZ_at = np.random.randn(MAXITERS, NN, dd)
+    SS_at = np.zeros((MAXITERS, NN, dd))
+    VV_at = np.zeros((MAXITERS, NN, dd))
+    for ii in range(NN):
+        SS_at[0, ii] = phi(ZZ_at[0, ii])
+        VV_at[0, ii] = grad_function_2(ZZ_at[0, ii], SS_at[0, ii], delta)
+
+
+    cost_at = np.zeros((MAXITERS))
+    gradients_norm = np.zeros((MAXITERS))
+    alpha = 1e-2
+
     COMM_TIME = 5e-2
 
     node_list = []
 
-    for i in range(NN):
-        N_ii = np.nonzero(Adj[:, i])[0].tolist()
-        index_ii = i * n_x + np.arange(n_x)
-        x_init_i = x_init[index_ii].tolist()
-        distances_ii = distances[:, i].tolist()
-
+    for ii in range(NN):
+        #print(ii)
+        N_ii = np.nonzero(Adj[ii])[0]
+        print(N_ii)
+        print(type(N_ii))
+        ZZ_at_0_ii = np.array(ZZ_at[0, ii])
+        print(ZZ_at_0_ii)
+        print(type(ZZ_at_0_ii))
+        SS_at_0_ii = np.array(SS_at[0, ii])
+        print(SS_at_0_ii)
+        print(type(SS_at_0_ii))
+        VV_at_0_ii = np.array(VV_at[0, ii])
+        print(VV_at_0_ii)
+        print(type(VV_at_0_ii))
+        r_ii = np.array(r[ii])
+        print(r_ii)
+        print(type(r_ii))
+        AA_ii = AA[ii, ii]
+        print(AA_ii)
+        print(type(AA_ii))
+        print(gamma)
+        print(type(gamma))
+        print(delta)
+        print(type(delta))
+        print(alpha)
+        print(type(alpha))
+        AA_neighbors = []
+        for neighbor in N_ii:
+            AA_neighbors.append(AA[ii, neighbor])
+        print(AA_neighbors)
+        print(type(AA_neighbors))
         node_list.append(
             Node(
                 package="formation_control",
-                namespace=f"agent_{i}",
+                namespace=f"agent_{ii}",
                 executable="generic_agent",
                 parameters=[
                     {
-                        "id": i,
+                        "id": ii,
                         "communication_time": COMM_TIME,
-                        "neighbors": N_ii,
-                        "xzero": x_init_i,
-                        "dist": distances_ii,
+                        "neighbors": N_ii.tolist(),
+                        "zz_init": ZZ_at_0_ii.tolist(),
+                        "ss_init": SS_at_0_ii.tolist(),
+                        "vv_init": VV_at_0_ii.tolist(),
+                        "r": r_ii.tolist(),
+                        "gamma": gamma,
+                        "delta": delta,
+                        "aplha": alpha,
+                        "AA": AA_ii,
+                        "AA_neighbors": AA_neighbors,
                         "maxT": MAXITERS,
                     }
                 ],
                 output="screen",
-                prefix=f'xterm -title "agent_{i}" -hold -e',
+                prefix=f'xterm -title "agent_{ii}" -hold -e',
             )
         )
 
